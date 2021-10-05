175af7a3e5747792908f229f19cfc48c204c3eb2
Mosholm <Oliv387u@stud.kea.dk>
2021-10-05 19:33:33 +0200

    Handwriting and preloader added

---
+++ script.js
@@ -0,0 +1,16 @@
+// pageloader site
+
+window.addEventListener("load", () => {
+    console.log('loaded');
+    const preloader = document.querySelector(".preloaderBg");
+    preloader.classList.add("done");
+})
+
+
+
+
+// // Hand writing
+// Get the exact lenght, of the stroct by typing this in the console
+
+            // p=document.getElementById('first');
+            // p.getTotalLength()

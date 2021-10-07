2c05060a0212024f4000f85e770eb9d8dd245409
Mosholm <Oliv387u@stud.kea.dk>
2021-10-07 11:07:45 +0200

    Guide started

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
 // pageloader site
 
 window.addEventListener("load", () => {
-    console.log('loaded');
-    const preloader = document.querySelector(".preloaderBg");
-    preloader.classList.add("done");
-})
-
-
-
+  console.log("loaded");
+  const preloader = document.querySelector(".preloaderBg");
+  preloader.classList.add("done");
+});
 
 // // Hand writing
 // Get the exact lenght, of the stroct by typing this in the console
 
-            // p=document.getElementById('first');
-            // p.getTotalLength()
+// p=document.getElementById('first');
+// p.getTotalLength()
 
 /*download button*/
 
-document.addEventListener("click", function(){
-    var collapse =document.getElementById('container');
-    collapse.classList.toggle("collapse");
+document.addEventListener("click", function () {
+  var collapse = document.getElementById("container");
+  collapse.classList.toggle("collapse");
 
-    var newline =document.getElementById('newLine');
-    newLine.classList.add("show");
-    newline.classList.remove("hidden");
+  var newline = document.getElementById("newLine");
+  newLine.classList.add("show");
+  newline.classList.remove("hidden");
 
-    var remove =document.getElementById('container');
-    remove.classList.add("hidden");
-    
-})
+  var remove = document.getElementById("container");
+  remove.classList.add("hidden");
+});
